70e26a1001ee80cf34560d2dc6bb5e4d6b2166a3
ac360 <austencollins@gmail.com>
2021-01-29 13:06:49 -0800

    add retry logic and bump version

--- src/listAllCloudFormationStacksInARegion.js
+++ src/listAllCloudFormationStacksInARegion.js
@@ -1,16 +1,30 @@
 const AWS = require('aws-sdk')
+const utils = require('./utils')
 
 const listAllCloudFormationStacksInARegion = async (
   config,
   {
     region = null,
     nextToken = null,
-    stackStatusFilter = null
+    stackStatusFilter = null,
+    retry = 0
   },
   stacks = []) => {
 
   if (!region) {
     throw new Error(`Missing "region" param.`)
+  }
+
+  // If this is a retry attempt, and it's greater than 6, throw an error
+  if (retry > 6) {
+    throw new Error('AWS CloudFormation Rate Limit has been exceeded and retrying 6 times has failed.')
+  }
+
+  // If this is a retry attempt, add a sleep, and jitter it
+  if (retry > 0) {
+    let randomMs = Math.random() * (0.8 - 3.5) + 3.5
+    randomMs = parseInt(randomMs * 1000)
+    await utils.sleep(randomMs)
   }
 
   // Override region configuration
@@ -33,7 +47,27 @@ const listAllCloudFormationStacksInARegion = async (
     StackStatusFilter: stackStatusFilter,
   }
 
-  const res = await cloudformation.listStacks(params).promise();
+  let res
+  try {
+    res = await cloudformation.listStacks(params).promise();
+  } catch (error) {
+    // Retry, if retryable
+    if ((error.message && error.message.includes('Rate exceeded')) ||
+      error.code === 'Throttling' ||
+      error.retryable === true) {
+      return await listAllCloudFormationStacksInARegion(
+        config,
+        {
+          region,
+          nextToken,
+          stackStatusFilter,
+          retry: retry + 1,
+        },
+        stacks
+      );
+    }
+    throw error
+  }
 
   // Concatenate stacks
   if (res.StackSummaries && res.StackSummaries.length) {
